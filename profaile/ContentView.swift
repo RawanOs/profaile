b81148aeec595555012f38f68179527d8ddbc659
RawanOs <89946063+RawanOs@users.noreply.github.com>
2023-10-24 20:42:01 +0300

    profaile

--- profaile/ContentView.swift
+++ profaile/ContentView.swift
@@ -9,18 +9,34 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack{
+            Color.indigo
+            VStack {
+                HStack{
+                    Image("Image")
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 150, height: 150)
+                        .padding(4)
+                        .overlay(
+                            Circle()
+                                .stroke(Color.white, lineWidth: 1)
+                        )
+                        .clipShape(Circle())
+                    VStack{
+                        Text("tariq")
+                        Text("ios swiftUI Engineer")}
+                }.foregroundColor(.white)
+                Text("i work as ios engenner to make ios apps, ")
+                    .foregroundColor(.white)
+            }
+               
+        }.ignoresSafeArea()
+    }
+}
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
         }
-        .padding()
     }
-}
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
-}
