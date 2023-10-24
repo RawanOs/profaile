9d683684016b4a95af124769b7411faad0553204
RawanOs <89946063+RawanOs@users.noreply.github.com>
2023-10-24 19:59:08 +0300

    Initial Commit

---
+++ profaile/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  profaile
+//
+//  Created by Rawan on 09/04/1445 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ profaile/profaileApp.swift
@@ -0,0 +1,17 @@
+//
+//  profaileApp.swift
+//  profaile
+//
+//  Created by Rawan on 09/04/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct profaileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
